0325bd59a6d9d8f16d32e33d61ea63c32511a687
yusasa16 <yusasa16@gmail.com>
2023-08-06 18:35:21 +0900

    heading2作成

--- src/components/ui/Heading2/index.tsx
+++ src/components/ui/Heading2/index.tsx
@@ -5,24 +5,6 @@ interface Heading2Props {
 	subText?: string;
 }
 
-const subTextCss = css({
-	display: "block",
-	color: "textDarkAccent",
-	fontSize: "1.6rem",
-	lineHeight: "calc(32/16)"
-});
-
-const mainTextCss = css({
-	color: "textDark",
-	fontSize: "4rem",
-	fontWeight: "900",
-	lineHeight: "calc(48/40)",
-	md: {
-		fontSize: "5.6rem",
-		lineHeight: "calc(64/56)"
-	}
-});
-
 export default function Heading2(props: Heading2Props) {
 	return (
 		<div className={css({ textAlign: "center" })}>
@@ -35,3 +17,21 @@ export default function Heading2(props: Heading2Props) {
 		</div>
 	);
 }
+
+const subTextCss = css({
+	display: "block",
+	color: "textDarkAccent",
+	fontSize: "1.6rem",
+	lineHeight: "calc(32/16)",
+});
+
+const mainTextCss = css({
+	color: "textDark",
+	fontSize: "4rem",
+	fontWeight: "900",
+	lineHeight: "calc(48/40)",
+	md: {
+		fontSize: "5.6rem",
+		lineHeight: "calc(64/56)",
+	},
+});
