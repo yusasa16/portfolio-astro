336cd8c6d0d05703010243cbc555c44ce8e95af6
yusasa16 <yusasa16@gmail.com>
2023-08-19 14:53:43 +0900

    ディレクトリ階層もろもろ見直し

--- panda.config.ts
+++ panda.config.ts
@@ -14,35 +14,7 @@ export default defineConfig({
 	theme: {
 		extend: {},
 		tokens: {
-			colors: {
-				accent: {
-					value: "#EF6C57",
-				},
-				bg01: {
-					value: "#28293E",
-				},
-				bg02: {
-					value: "#FDF0E9",
-				},
-				textDark: {
-					value: "#391400",
-				},
-				textDarkAccent: {
-					value: "#EF6D58",
-				},
-				textDarkGray: {
-					value: "#391400A3",
-				},
-				textLight: {
-					value: "#FFFFFF",
-				},
-				textLightAccent: {
-					value: "#EF6D58",
-				},
-				textLightGray: {
-					value: "#FFFFFFA3",
-				},
-			},
+			colors: {},
 		},
 	},
 
---
+++ src/components/elements/Heading2.tsx
@@ -0,0 +1,20 @@
+import { css } from "../../../styled-system/css";
+
+interface Heading2Props {
+	text: string;
+	level?: number;
+}
+
+export default function Heading2(props: Heading2Props) {
+	return (
+		<h2
+			className={css({
+				textAlign: "center",
+				fontSize: "2.4rem",
+				fontWeight: "bold",
+			})}
+		>
+			{props.text}
+		</h2>
+	);
+}
