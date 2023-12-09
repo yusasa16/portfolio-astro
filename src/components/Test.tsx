206bff71e17d28361c31edcdff27f09c60a188e9
yusasa16 <yusasa16@gmail.com>
2023-12-09 18:42:59 +0900

    storybook削除

--- src/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-import { css } from '../../styled-system/css';
-
-interface TestProps {
-	text: string;
-}
-
-export default function Test(props: TestProps) {
-	return <h2 className={css({ bg: 'bg02' })}>{props.text}</h2>;
-}
