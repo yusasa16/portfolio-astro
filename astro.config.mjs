d64be65405ce7cd6387d433dd660236e1eb4ec17
yusasa16 <yusasa16@gmail.com>
2023-12-31 00:10:11 +0900

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import nodejs from '@astrojs/node';
+// import nodejs from '@astrojs/node';
 
 import react from '@astrojs/react';
 
@@ -7,7 +7,13 @@ import pandacss from '@pandacss/dev/astro';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	adapter: nodejs({ mode: 'middleware' }),
+	// output: 'hybrid',
+	// adapter: nodejs({ mode: 'middleware' }),
 	integrations: [react(), pandacss()],
+	redirects: {
+		'/blog/': {
+			status: 302,
+			destination: '/blog/pages/1',
+		},
+	},
 });
