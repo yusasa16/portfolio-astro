095b3af731dcb8e3c8a786f10628b0b885a67457
yusasa16 <yusasa16@gmail.com>
2024-03-16 13:38:39 +0900

    tailwindcss導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 // import nodejs from '@astrojs/node';
-
 import react from '@astrojs/react';
-
 import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	// output: 'hybrid',
 	// adapter: nodejs({ mode: 'middleware' }),
-	integrations: [react(), alpinejs({ entrypoint: '/src/entrypoint' })],
+	integrations: [react(), alpinejs({ entrypoint: '/src/entrypoint' }), tailwind()],
 	redirects: {
 		'/blog/': {
 			status: 302,
